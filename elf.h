c7152a5dd1ff0b4702f3c126ac9b2101819d3150
Eli Array Minkoff <eli@earrayminkoff.tech>
2024-05-16 20:48:19 -0400

    Rearrange whitespace for Ehdr-related macros
    This makes it more consistent with my preferred code style

--- elf.h
+++ elf.h
@@ -69,68 +69,63 @@ typedef Elf64_Half Elf64_Versym;
 
 typedef struct
 {
-  unsigned char e_ident[EI_NIDENT];     /* Magic number and other info */
-  Elf64_Half    e_type;                 /* Object file type */
-  Elf64_Half    e_machine;              /* Architecture */
-  Elf64_Word    e_version;              /* Object file version */
-  Elf64_Addr    e_entry;                /* Entry point virtual address */
-  Elf64_Off     e_phoff;                /* Program header table file offset */
-  Elf64_Off     e_shoff;                /* Section header table file offset */
-  Elf64_Word    e_flags;                /* Processor-specific flags */
-  Elf64_Half    e_ehsize;               /* ELF header size in bytes */
-  Elf64_Half    e_phentsize;            /* Program header table entry size */
-  Elf64_Half    e_phnum;                /* Program header table entry count */
-  Elf64_Half    e_shentsize;            /* Section header table entry size */
-  Elf64_Half    e_shnum;                /* Section header table entry count */
-  Elf64_Half    e_shstrndx;             /* Section header string table index */
+  unsigned char e_ident[EI_NIDENT]; /* Magic number and other info */
+  Elf64_Half    e_type;             /* Object file type */
+  Elf64_Half    e_machine;          /* Architecture */
+  Elf64_Word    e_version;          /* Object file version */
+  Elf64_Addr    e_entry;            /* Entry point virtual address */
+  Elf64_Off     e_phoff;            /* Program header table file offset */
+  Elf64_Off     e_shoff;            /* Section header table file offset */
+  Elf64_Word    e_flags;            /* Processor-specific flags */
+  Elf64_Half    e_ehsize;           /* ELF header size in bytes */
+  Elf64_Half    e_phentsize;        /* Program header table entry size */
+  Elf64_Half    e_phnum;            /* Program header table entry count */
+  Elf64_Half    e_shentsize;        /* Section header table entry size */
+  Elf64_Half    e_shnum;            /* Section header table entry count */
+  Elf64_Half    e_shstrndx;         /* Section header string table index */
 } Elf64_Ehdr;
 
 /* Fields in the e_ident array.  The EI_* macros are indices into the
    array.  The macros under each EI_* macro are the values the byte
    may have.  */
 
-#define EI_MAG0         0               /* File identification byte 0 index */
-#define ELFMAG0         0x7f            /* Magic number byte 0 */
+#define EI_MAG0         0       /* File identification byte 0 index */
+#define ELFMAG0         0x7f    /* Magic number byte 0 */
 
-#define EI_MAG1         1               /* File identification byte 1 index */
-#define ELFMAG1         'E'             /* Magic number byte 1 */
+#define EI_MAG1         1       /* File identification byte 1 index */
+#define ELFMAG1         'E'     /* Magic number byte 1 */
 
-#define EI_MAG2         2               /* File identification byte 2 index */
-#define ELFMAG2         'L'             /* Magic number byte 2 */
+#define EI_MAG2         2       /* File identification byte 2 index */
+#define ELFMAG2         'L'     /* Magic number byte 2 */
 
-#define EI_MAG3         3               /* File identification byte 3 index */
-#define ELFMAG3         'F'             /* Magic number byte 3 */
+#define EI_MAG3         3       /* File identification byte 3 index */
+#define ELFMAG3         'F'     /* Magic number byte 3 */
 
-#define EI_CLASS        4               /* File class byte index */
-#define ELFCLASS64      2               /* 64-bit objects */
+#define EI_CLASS        4       /* File class byte index */
+#define ELFCLASS64      2       /* 64-bit objects */
 
-#define EI_DATA         5               /* Data encoding byte index */
-#define ELFDATA2LSB     1               /* 2's complement, little endian */
+#define EI_DATA         5       /* Data encoding byte index */
+#define ELFDATA2LSB     1       /* 2's complement, little endian */
 
-#define EI_VERSION      6               /* File version byte index */
-                                        /* Value must be EV_CURRENT */
+#define EI_VERSION      6       /* File version byte index */
+                                /* Value must be EV_CURRENT */
 
-#define EI_OSABI        7               /* OS ABI identification */
-#define ELFOSABI_SYSV   0               /* UNIX System V ABI */
+#define EI_OSABI        7       /* OS ABI identification */
+#define ELFOSABI_SYSV   0       /* UNIX System V ABI */
 
-#define EI_ABIVERSION   8               /* ABI version */
-
-#define EI_PAD          9               /* Byte index of padding bytes */
+#define EI_ABIVERSION   8       /* ABI version */
+#define EI_PAD          9       /* Byte index of padding bytes */
 
 /* Legal values for e_type (object file type).  */
-
-#define ET_EXEC         2               /* Executable file */
+#define ET_EXEC         2       /* Executable file */
 
 /* Legal values for e_machine (architecture).  */
-
 #define EM_X86_64       62      /* AMD x86-64 architecture */
 
 /* Legal values for e_version (version).  */
-
-#define EV_CURRENT      1               /* Current version */
+#define EV_CURRENT      1       /* Current version */
 
 /* Section header.  */
-
 
 typedef struct
 {
