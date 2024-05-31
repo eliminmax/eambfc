ced44b5924e8f4778225cb89fda462980520a745
Eli Array Minkoff <eli@earrayminkoff.tech>
2024-05-30 22:15:51 -0400

    JSON-escape filename and error message (-j output)

---
+++ json_escape.c
@@ -0,0 +1,88 @@
+/* SPDX-FileCopyrightText: 2024 Eli Array Minkoff
+ *
+ * SPDX-License-Identifier: GPL-3.0-only */
+
+/* C99 */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/* POSIX */
+#include <unistd.h>
+
+/* return a pointer to a JSON-escaped version of the input string
+ * calling function is responsible for freeing it */
+char *json_escape(char* str) {
+    size_t bufsz = 1; /* start with 1 for the null terminator at the end */
+    char *p = str;
+    /* 2 passes - 1 to determine bufsz, and 1 to actually build the string. */
+    while (*p) {
+        switch(*p) {
+          case '\n':
+          case '\r':
+          case '\f':
+          case '\t':
+          case '\b':
+          case '\\':
+          case '\"':
+            bufsz += 2;
+            break;
+          default:
+        /* was going to be a switch statement, but case a ... d is non-portable. 
+         * Instead, I went with this ugly hybrid system */
+            if (*p < 040) {
+                bufsz += 6;
+            } else {
+                bufsz++;
+            }
+            break;
+        }
+        p++;
+    }
+    char *json_escaped = (char *)malloc(bufsz);
+    char *outp = json_escaped;
+    if (json_escaped == NULL) return "malloc failed for json_escape";
+    p = str;
+    while (*p) {
+        switch(*p) {
+          case '\n':
+            *(outp++) = '\\';
+            *(outp++) = 'n';
+            break;
+          case '\r':
+            *(outp++) = '\\';
+            *(outp++) = 'r';
+            break;
+          case '\f':
+            *(outp++) = '\\';
+            *(outp++) = 'f';
+            break;
+          case '\t':
+            *(outp++) = '\\';
+            *(outp++) = 't';
+            break;
+          case '\b':
+            *(outp++) = '\\';
+            *(outp++) = 'b';
+            break;
+          case '\\':
+            *(outp++) = '\\';
+            *(outp++) = '\\';
+            break;
+          case '\"':
+            *(outp++) = '\\';
+            *(outp++) = '\"';
+            break;
+          default:
+            if (*p < 040) {
+                sprintf(outp, "\\u%04hhx", *p);
+                outp += 6;
+            } else {
+                *(outp++) = *p;
+            }
+            break;
+        }
+        p++;
+    }
+    *outp = 0;
+    return json_escaped;
+}
--- main.c
+++ main.c
@@ -17,8 +17,8 @@
 #include <sys/types.h>
 #include <unistd.h>
 /* internal */
-#include "eam_compile.h"
 #include "json_escape.h"
+#include "eam_compile.h"
 
 /* Return the permission mask to use for the output file */
 mode_t _getperms(void) {
@@ -110,6 +110,10 @@ int main(int argc, char* argv[]) {
     int result;
     int opt;
     int ret = EXIT_SUCCESS;
+    char *outname;
+    char *errorMsgJSON;
+    char *outnameJSON;
+    char *filenameJSON;
     /* default to empty string. */
     char *ext = "";
     /* default to false, set to true if relevant argument was passed. */
@@ -187,40 +191,50 @@ int main(int argc, char* argv[]) {
     }
 
     for (/* reusing optind here */; optind < argc; optind++) {
+        outname = (char *)malloc(strlen(argv[optind]) + 1);
+        if (outname == NULL) {
+            showerror("malloc failure!\n");
+            exit(EXIT_FAILURE);
+        }
+        if (json) {
+            filenameJSON = json_escape(argv[optind]);
+            outnameJSON = json_escape(outname);
+        }
+        strcpy(outname, argv[optind]);
         srcFD = open(argv[optind], O_RDONLY);
         if (srcFD < 0) {
             if (json) {
                 printf(
                     "{\"errorId\":\"OPEN_R_FAILED\",\"file\":\"%s\"}\n",
-                    argv[optind]
+                    filenameJSON
                 );
             } else {
                 showerror("Failed to open %s for reading.\n", argv[optind]);
             }
             filefail();
         }
-        if (! _rmext(argv[optind], ext)) {
+        if (! _rmext(outname, ext)) {
             if (json) {
                 printf(
-                    "{\"errorId\":\"BAD_EXTENSION\",\"file\":\"%s%s\"}\n",
-                    argv[optind], ext
+                    "{\"errorId\":\"BAD_EXTENSION\",\"file\":\"%s\"}\n",
+                    argv[optind]
                 );
             } else {
                 showerror("%s does not end with %s.\n", argv[optind], ext);
             }
             filefail();
         }
-        dstFD = open(argv[optind], O_WRONLY+O_CREAT+O_TRUNC, permissions);
+        dstFD = open(outname, O_WRONLY+O_CREAT+O_TRUNC, permissions);
         if (dstFD < 0) {
             if (json) {
                 printf(
-                    "{\"errorId\":\"OPEN_W_FAILED\",\"file\":\"%s%s\"}\n",
-                    argv[optind], ext
+                    "{\"errorId\":\"OPEN_W_FAILED\",\"file\":\"%s\"}\n",
+                    outname
                 );
             } else {
                 showerror(
                     "Failed to open destination file %s for writing.\n",
-                    argv[optind]
+                    outname
                 );
             }
             if (moveahead) {
@@ -235,20 +249,23 @@ int main(int argc, char* argv[]) {
         if (!result) {
             for(uint8_t i = 0; i < MAX_ERROR && ErrorList[i].active; i++) {
                 if (json) {
+                    errorMsgJSON = json_escape(ErrorList[i].errorMessage);
                     printf(
-                        "{\"errorId\":\"%s\",\"file\":\"%s%s\",\"line\":%d,"
-                        "\"column\":%d}\n",
+                        "{\"errorId\":\"%s\",\"file\":\"%s\",\"line\":%d,"
+                        "\"column\":%d,\"message\":\"%s\"}\n",
                         ErrorList[i].errorId,
-                        argv[optind], ext,
+                        argv[optind],
                         ErrorList[i].currentInstructionLine,
-                        ErrorList[i].currentInstructionColumn
+                        ErrorList[i].currentInstructionColumn,
+                        errorMsgJSON
                     );
+                    free(errorMsgJSON);
                 } else {
                     showerror(
-                        "%s%s: Failed to compile '%c' at line %d, column %d.\n"
+                        "%s: Failed to compile '%c' at line %d, column %d.\n"
                         "Error ID: %s\n"
                         "Error message: \"%s\"\n",
-                        argv[optind], ext,
+                        argv[optind],
                         ErrorList[i].currentInstruction,
                         ErrorList[i].currentInstructionLine,
                         ErrorList[i].currentInstructionColumn,
@@ -257,9 +274,14 @@ int main(int argc, char* argv[]) {
                     );
                 }
             }
-            if (!keep) remove(argv[optind]);
+            if (!keep) remove(outname);
             filefail();
         }
+        if (json) {
+            free(filenameJSON);
+            free(outnameJSON);
+        }
+        free(outname);
     }
 
     return ret;
