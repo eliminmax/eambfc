eliminmax/eambfc

commit ba858eb14d5952dc9f10c088e7b266ea9cc744b0

Re-engineer version flags

Makefile
@@ -1,15 +1,16 @@
 # SPDX-FileCopyrightText: 2024 Eli Array Minkoff
 #
-# SPDX-License-Identifier: 0BSD
+# SPDX-License-Identifier: GPL-3.0-only
 .POSIX:
 
 PREFIX ?= /usr/local
 
-CFLAGS += -D _POSIX_C_SOURCE=200809L
-
 # POSIX standard tool to compile C99 code, could be any C99-compatible compiler
 # which supports the POSIX-specified options
 CC ?= c99
+
+# Enable POSIX.1-2008 C features and headers
+CFLAGS += -D _POSIX_C_SOURCE=200809L
 
 # Compile-time configuration values
 MAX_ERROR ?= 32
@@ -24,11 +25,19 @@ install: eambfc
 	mkdir -p $(DESTDIR)$(PREFIX)/bin
 	cp -f eambfc $(DESTDIR)$(PREFIX)/bin
 
-config.h:
+config.h config.template.h:
+	if command -v git >/dev/null && [ -e .git ]; then \
+		git_commit="$$(git log -n1 --pretty=format:'git commit: %h')"; \
+	else \
+		git_commit='Not built from git repo'; \
+	fi; \
 	sed -e '/MAX_ERROR/s/@@/$(MAX_ERROR)/' \
 		-e '/TAPE_BLOCKS/s/@@/$(TAPE_BLOCKS)/' \
 		-e '/MAX_NESTING_LEVEL/s/@@/$(MAX_NESTING_LEVEL)/' \
 		-e "/EAMBFC_VERSION/s/@@/\"$$(cat version)\"/" \
+		-e '/EAMBFC_CC/s/@@/"$(CC)"/' \
+		-e '/EAMBFC_CFLAGS/s/@@/"$(CFLAGS)"/' \
+		-e "/EAMBFC_COMMIT/s/@@/\"$$git_commit\"/" \
 		<config.template.h >config.h
 
 serialize.o: serialize.c
@@ -54,9 +63,8 @@ can-run-linux-amd64: mini_elf
 test: can-run-linux-amd64 eambfc
 	(cd tests; make clean test)
 
-
 multibuild: config.h
-	env SKIP_TEST=y ./multibuild.sh
+	env SKIP_TEST=y ./multibuild.svh
 multibuild-test: can-run-linux-amd64 config.h
 	./multibuild.sh
 

config.template.h
@@ -23,4 +23,12 @@
 /* The current version */
 #define EAMBFC_VERSION @@
 
+/* The current git commit */
+#define EAMBFC_COMMIT @@
+
+/* The C compiler and build flags */
+#define EAMBFC_CC @@
+#define EAMBFC_CFLAGS @@
+
 #endif /* EAM_CONFIG_H */
+/* vim:set ft=cpp: */

main.c
@@ -127,13 +127,27 @@ int main(int argc, char* argv[]) {
             return EXIT_SUCCESS;
           case 'V':
             printf(
-                "%s: eambfc version %s\ntape size: %d 4-KiB blocks\n"\
-                "maximum nesting level: %d\nmaximum errors: %d\n",
+                "%s: eambfc version %s\n"
+                "Copyright (c) 2024 Eli Array Minkoff.\n"
+                "License: GNU GPL version 3 "
+                "<https://gnu.org/licenses/gpl.html>.\n"
+                "This is free software: "
+                "you are free to change and redistribute it.\n"
+                "There is NO WARRANTY, to the extent permitted by law.\n"
+                "tape size: %d 4-KiB blocks\n"
+                "max nesting level: %d\n"
+                "max compiler errors shown: %d\n"
+                "compiler: %s\n"
+                "compiler flags: %s\n"
+                "%s\n", /* git info or message stating git not used. */
                 argv[0],
                 EAMBFC_VERSION,
                 TAPE_BLOCKS,
                 MAX_NESTING_LEVEL,
-                MAX_ERROR
+                MAX_ERROR,
+                EAMBFC_CC,
+                EAMBFC_CFLAGS,
+                EAMBFC_COMMIT
             );
             return EXIT_SUCCESS;
           case 'q':

commit 10dae9624a96138238fb012a56fee41a9b6a5e61

Fix typo that broke multibuild target in ba858eb1

Makefile
@@ -66,7 +66,7 @@ test: can-run-linux-amd64 eambfc
 	(cd tests; make clean test)
 
 multibuild: config.h
-	env SKIP_TEST=y ./multibuild.svh
+	env SKIP_TEST=y ./multibuild.sh
 multibuild-test: can-run-linux-amd64 config.h
 	./multibuild.sh
 