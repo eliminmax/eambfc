2d93210f9edd3c3f1c7108554cc7a70ae496adb9
Eli Array Minkoff <eli@planetminkoff.com>
2024-06-10 12:09:44 -0400

    Add implementations of condensed instructions

--- x86_64_encoders.h
+++ x86_64_encoders.h
@@ -12,44 +12,44 @@
 #include <stdint.h>
 
 /* used for making system calls, setup, and other miscellaneous things */
-bool eamAsmSetReg(unsigned char reg, int32_t imm, int fd, off_t *sz);
-bool eamAsmRegCopy(unsigned char dst, unsigned char src, int fd, off_t *sz);
+bool eamAsmSetReg(uint8_t reg, int32_t imm, int fd, off_t *sz);
+bool eamAsmRegCopy(uint8_t dst, uint8_t src, int fd, off_t *sz);
 bool eamAsmSyscall(int fd, off_t *sz);
 
 /* ] */
-bool eamAsmJumpNotZero(unsigned char reg, int32_t offset, int fd, off_t *sz);
+bool eamAsmJumpNotZero(uint8_t reg, int32_t offset, int fd, off_t *sz);
 /* [ */
-bool eamAsmJumpZero(unsigned char reg, int32_t offset, int fd, off_t *sz);
+bool eamAsmJumpZero(uint8_t reg, int32_t offset, int fd, off_t *sz);
 /* > */
-bool eamAsmIncReg(unsigned char reg, int fd, off_t *sz);
+bool eamAsmIncReg(uint8_t reg, int fd, off_t *sz);
 /* < */
-bool eamAsmDecReg(unsigned char reg, int fd, off_t *sz);
+bool eamAsmDecReg(uint8_t reg, int fd, off_t *sz);
 /* + */
-bool eamAsmIncByte(unsigned char reg, int fd, off_t *sz);
+bool eamAsmIncByte(uint8_t reg, int fd, off_t *sz);
 /* - */
-bool eamAsmDecByte(unsigned char reg, int fd, off_t *sz);
+bool eamAsmDecByte(uint8_t reg, int fd, off_t *sz);
 
-/* TODO: UNIMPLEMENTED machine code for extra instructions from eambfc-ir */
+/* machine code for extra instructions from eambfc-ir */
 /* } */
-bool eamAsmAddRegByte(unsigned char reg, int8_t imm8, int fd, off_t *sz);
+bool eamAsmAddRegByte(uint8_t reg, int8_t imm8, int fd, off_t *sz);
 /* { */
-bool eamAsmSubRegByte(unsigned char reg, int8_t imm8, int fd, off_t *sz);
+bool eamAsmSubRegByte(uint8_t reg, int8_t imm8, int fd, off_t *sz);
 /* ) */
-bool eamAsmAddRegWord(unsigned char reg, int16_t imm16, int fd, off_t *sz);
+bool eamAsmAddRegWord(uint8_t reg, int16_t imm16, int fd, off_t *sz);
 /* ( */
-bool eamAsmSubRegWord(unsigned char reg, int16_t imm16, int fd, off_t *sz);
+bool eamAsmSubRegWord(uint8_t reg, int16_t imm16, int fd, off_t *sz);
 /* $ */
-bool eamAsmAddRegDoubWord(unsigned char reg, int32_t imm32, int fd, off_t *sz);
+bool eamAsmAddRegDoubWord(uint8_t reg, int32_t imm32, int fd, off_t *sz);
 /* ^ */
-bool eamAsmSubRegDoubWord(unsigned char reg, int32_t imm32, int fd, off_t *sz);
+bool eamAsmSubRegDoubWord(uint8_t reg, int32_t imm32, int fd, off_t *sz);
 /* n */
-bool eamAsmAddRegQuadWord(unsigned char reg, int64_t imm64, int fd, off_t *sz);
+bool eamAsmAddRegQuadWord(uint8_t reg, int64_t imm64, int fd, off_t *sz);
 /* N */
-bool eamAsmSubRegQuadWord(unsigned char reg, int64_t imm64, int fd, off_t *sz);
+bool eamAsmSubRegQuadWord(uint8_t reg, int64_t imm64, int fd, off_t *sz);
 /* # */
-bool eamAsmAddMem(unsigned char rec, int8_t imm8, int fd, off_t *sz);
+bool eamAsmAddMem(uint8_t rec, int8_t imm8, int fd, off_t *sz);
 /* = */
-bool eamAsmSubMem(unsigned char rec, int8_t imm8, int fd, off_t *sz);
+bool eamAsmSubMem(uint8_t rec, int8_t imm8, int fd, off_t *sz);
 /* @ */
-bool eamAsmSetMemZero(unsigned char reg, int fd, off_t *sz);
+bool eamAsmSetMemZero(uint8_t reg, int fd, off_t *sz);
 #endif /* INSTRUCTION_ENCODERS_H */
