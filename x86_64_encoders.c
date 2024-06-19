9541ba56e381f33f3f11846978d1ee20966c1f50
Eli Array Minkoff <eli@earrayminkoff.tech>
2024-06-19 13:43:28 -0400

    lightly refactor x86_64_encoders.c

--- x86_64_encoders.c
+++ x86_64_encoders.c
@@ -14,7 +14,7 @@
 /* internal */
 #include "serialize.h"
 
-/* if there are more than 3 lines in common between equivalent ADD/SUB or JZ/JNZ
+/* if there are more than 3 lines in common between similar ADD/SUB or JZ/JNZ
  * eamAsm functions, the common lines dealing with writing machine code should
  * be moved into a static inline function. */
 
@@ -30,34 +30,32 @@ bool eamAsmSyscall(int fd, off_t *sz) {
     return write(fd, (uint8_t[]){ 0x0f, 0x05 }, 2) == 2;
 }
 
-/* TEST byte [reg], 0xff */
-static inline bool eamAsmJumpTest(uint8_t reg, int fd) {
-    return write(fd, (uint8_t[]){ 0xf6, reg, 0xff}, 3) == 3;
-}
-
 /* TEST byte [reg], 0xff; Jcc|tttn jmp_offset */
-static inline bool eamAsmJcc(char tttn, uint8_t reg, int32_t offset, int fd) {
-    uint8_t i_bytes[6] = { 0x0f, 0x80|tttn, 0x00, 0x00, 0x00, 0x00 };
-    /* need to test the byte first */
-    if (!eamAsmJumpTest(reg, fd)) return false;
+static inline bool x86TestJcc(char tttn, uint8_t reg, int32_t offset, int fd) {
+    uint8_t i_bytes[9] = {
+        /* TEST byte [reg], 0xff */
+        0xf6, reg, 0xff,
+        /* Jcc|tttn jmp_offset */
+        0x0f, 0x80 | tttn, 0x00, 0x00, 0x00, 0x00
+    };
     /* need to cast to uint32_t for serialize32.
      * Acceptable as POSIX requires 2's complement for signed types. */
-    if (serialize32(offset, (char *)&i_bytes[2]) != 4) return false;
-    return write(fd, &i_bytes, 6) == 6;
+    if (serialize32(offset, (char *)&i_bytes[5]) != 4) return false;
+    return write(fd, &i_bytes, 9) == 9;
 }
 
 /* TEST byte [reg], 0xff; JNZ jmp_offset */
 bool eamAsmJumpNotZero(uint8_t reg, int32_t offset, int fd, off_t *sz) {
     *sz += 9;
     /* Jcc with tttn=0b0101 is JNZ or JNE */
-    return eamAsmJcc(0x5, reg, offset, fd);
+    return x86TestJcc(0x5, reg, offset, fd);
 }
 
 /* TEST byte [reg], 0xff; JZ jmp_offset */
 bool eamAsmJumpZero(uint8_t reg, int32_t offset, int fd, off_t *sz) {
     *sz += 9;
     /* Jcc with tttn=0b0100 is JZ or JE */
-    return eamAsmJcc(0x4, reg, offset, fd);
+    return x86TestJcc(0x4, reg, offset, fd);
 }
 
 /* INC and DEC are encoded very similarly with very few differences between
@@ -179,7 +177,7 @@ bool eamAsmSubRegDoubWord(uint8_t reg, int32_t imm32, int fd, off_t *sz) {
     return write(fd, &i_bytes, 6) == 6;
 }
 
-static inline bool x86AddSubQW(uint8_t reg, int64_t imm64, int fd, uint8_t op) {
+static inline bool x64AddSubQW(uint8_t reg, int64_t imm64, int fd, uint8_t op) {
     /* There are no instructions to add or subtract a 64-bit immediate. Instead,
      * the approach  to use is first PUSH the value of a different register, MOV
      * the 64-bit immediate to that register, ADD/SUB that register to the
@@ -207,14 +205,14 @@ static inline bool x86AddSubQW(uint8_t reg, int64_t imm64, int fd, uint8_t op) {
 bool eamAsmAddRegQuadWord(uint8_t reg, int64_t imm64, int fd, off_t *sz) {
     *sz += 15;
     /* 0x00 is the opcode for register-to-register ADD */
-    return x86AddSubQW(reg, imm64, fd, 0x00);
+    return x64AddSubQW(reg, imm64, fd, 0x00);
 }
 
 /* N */
 bool eamAsmSubRegQuadWord(uint8_t reg, int64_t imm64, int fd, off_t *sz) {
     *sz += 15;
     /* 0x28 is the opcode for register-to-register SUB */
-    return x86AddSubQW(reg, imm64, fd, 0x28);
+    return x64AddSubQW(reg, imm64, fd, 0x28);
 }
 
 /* # */
