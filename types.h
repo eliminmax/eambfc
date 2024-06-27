bf2c68fef0b7e9080cc993a6a6d797a2938844c9
Eli Array Minkoff <eli@planetminkoff.com>
2024-06-27 12:22:09 -0400

    include extra type headers in types.h

--- types.h
+++ types.h
@@ -2,9 +2,17 @@
  *
  * SPDX-License-Identifier: GPL-3.0-only
  *
- * A much more readable way to write unsigned int, that makes it less likely for
- * lines to wind up too long or for code to be too unwieldy. */
+ * Types used throughout the eambfc codebase. */
 #ifndef EAMBFC_UINT
 #define EAMBFC_UINT 1
+/* C99 */
+#include <stdbool.h>
+/* internal */
+/* wrapper for inttypes.h that provides handling for missing uint64_t or int64_t
+ * types not guaranteed to be available in the POSIX.1-2008 or C99 standards.
+ *
+ * used in place of stdint.h or inttypes.h */
+#include "compat/eambfc_inttypes.h"
+
 typedef unsigned int uint;
 #endif /* EAMBFC_UINT */
