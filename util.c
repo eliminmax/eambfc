99578fb079f687dccaf54d1ca11f68cc83e83f82
Eli Array Minkoff <eli@planetminkoff.com>
2024-09-12 12:58:57 -0400

    Check if write size is too large to validate.

--- util.c
+++ util.c
@@ -4,14 +4,22 @@
  *
  * Miscellaneous utility functions used throughout the eambfc codebase. */
 
+/* C99 */
+#include <limits.h> /* SSIZE_MAX */
 /* POSIX */
-#include <unistd.h> /* write */
+#include <unistd.h> /* write, ssize_t */
 /* internal */
 #include "err.h" /* basic_err */
 
 
 bool write_obj(int fd, const void *buf, size_t sz) {
-    if (write(fd, buf, sz) != sz) {
+    if (sz > SSIZE_MAX) {
+        basic_err(
+            "WRITE_TOO_LARGE",
+            "Didn't write because write is too large to properly validate."
+        );
+    }
+    if (write(fd, buf, sz) != (ssize_t)sz) {
         basic_err("FAILED_WRITE", "Failed to write to file");
         return false;
     }
