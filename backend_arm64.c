f6f9537577c86a224fbfde769e6a3b48d252791a
Eli Array Minkoff <eli@planetminkoff.com>
2024-09-21 10:29:41 -0400

    Improve clarity of backend_arm64.c
    Mostly adds a bunch of comments, but also replaces a file-wide typedef
    struct with a normal struct within the only function that used it.

--- backend_arm64.c
+++ backend_arm64.c
@@ -1,7 +1,11 @@
 /* SPDX-FileCopyrightText: 2024 Eli Array Minkoff
  *
  * SPDX-License-Identifier: GPL-3.0-only
- * This file provides the arch_inter for the ARM64 architecture. */
+ * This file provides the arch_inter for the ARM64 architecture.
+ *
+ * Unlike the x86_64 backend, this is based on the Rust implementation, rather
+ * than the other way around. */
+
 /* POSIX */
 #include <unistd.h> /* write */
 /* internal */
@@ -11,6 +15,8 @@
 #include "types.h" /* uint*_t, int*_t, bool, off_t, size_t UINT64_C */
 #include "util.h" /* write_obj */
 
+/* in MOVK, MOVZ, and MOVN instructions, these correspond to the bits used
+ * within the 3rd byte to indicate shift level. */
 typedef enum {
     A64_SL_NO_SHIFT = 0x0,
     A64_SL_SHIFT16 = 0x20,
@@ -18,38 +24,41 @@ typedef enum {
     A64_SL_SHIFT48 = 0x60
 } shift_lvl;
 
+/* these byte values often appear within ADD and SUB instructions, and are the
+ * only difference between them. */
 typedef enum {
     A64_OP_ADD = 0x91,
     A64_OP_SUB = 0xd1
 } arith_op;
 
-typedef struct {
-    uint16_t imm;
-    shift_lvl shift;
-} shifted_imm;
-
+/* the final byte of each of the MOVK, MOVN, and MOVZ instructions are used as
+ * the corresponding enum values here */
 typedef enum {
     A64_MT_KEEP = 0xf2,
     A64_MT_ZERO = 0xd2,
     A64_MT_INVERT = 0x92
 } mov_type;
 
+/* For an instruction that takes 2 registers, OR their bit values into the
+ * appropriate parts of the machine code bytes in dst. */
 static void arm64_inject_reg_operands(uint8_t rt, uint8_t rn, uint8_t dst[4]) {
     dst[0] |= (rt | rn << 5);
     dst[1] |= (rn >> 3);
 }
 
+/* OR the immediate bytes for imm, shift, and x.reg into dst.
+ *
+ * This assumes that dst has everything except the register, shift, and
+ * immediate bits set, and that the immediate bits are set to zero. */
 static void arm64_inject_imm16_operands(
-    uint16_t imm,
-    shift_lvl shift,
-    uint8_t reg,
-    uint8_t *dst
+    uint16_t imm, shift_lvl shift, uint8_t reg, uint8_t *dst
 ) {
     dst[0] |= (reg | ((imm & 07) << 5));
     dst[1] |= (imm >> 3);
     dst[2] |= (shift | (imm >> 11));
 }
 
+/* set dst to the machine code for STRB w.aux, x.reg */
 static void arm64_store_to_byte(uint8_t reg, uint8_t aux, uint8_t dst[4]) {
     dst[0] = 0x00;
     dst[1] = 0x04;
@@ -58,6 +67,7 @@ static void arm64_store_to_byte(uint8_t reg, uint8_t aux, uint8_t dst[4]) {
     arm64_inject_reg_operands(aux, reg, dst);
 }
 
+/* set dst to the machine code for LDRB w.aux, x.reg */
 static void arm64_load_from_byte(uint8_t reg, uint8_t aux, uint8_t dst[4]) {
     dst[0] = 0x00;
     dst[1] = 0x04;
@@ -66,14 +76,17 @@ static void arm64_load_from_byte(uint8_t reg, uint8_t aux, uint8_t dst[4]) {
     arm64_inject_reg_operands(aux, reg, dst);
 }
 
+        /* return an scratch register that isn't the same as x.reg to use */
 static inline uint8_t arm64_aux_reg(uint8_t reg) {
-    /* return an auxiliary scratch register that isn't the same as reg to use */
     return (reg == 17) ? 16 : 17;
 }
 
+/* set dst to the machine code for one of MOVK, MOVN, or MOVZ, depending on mt,
+ * with the given operands. */
 static void arm64_mov(
     mov_type mt, uint16_t imm, shift_lvl shift, uint8_t reg, uint8_t *dst
 ) {
+    /* for MOVN, the bits need to be inverted. Ask Arm, not me. */
     uint16_t imm_bits = (mt == A64_MT_INVERT) ? ~imm : imm;
     dst[0] = 0x00;
     dst[1] = 0x00;
@@ -82,10 +95,13 @@ static void arm64_mov(
     arm64_inject_imm16_operands(imm_bits, shift, reg, dst);
 }
 
+/* Choose a combination of MOVZ, MOVK, and MOVN that sets register x.reg to
+ * the immediate imm */
 bool arm64_set_reg(uint8_t reg, int64_t imm, int fd, off_t *sz) {
     /* split the immediate into 4 16-bit parts - high, medium-high, medium-low,
      * and low. */
-    shifted_imm parts[4] = {
+    struct shifted_imm { uint16_t imm16; shift_lvl shift; };
+    struct shifted_imm parts[4] = {
         {(uint16_t)imm, A64_SL_NO_SHIFT},
         {(uint16_t)(imm >> 16), A64_SL_SHIFT16},
         {(uint16_t)(imm >> 32), A64_SL_SHIFT32},
@@ -100,27 +116,26 @@ bool arm64_set_reg(uint8_t reg, int64_t imm, int fd, off_t *sz) {
         default_val = 0;
         lead_mt = A64_MT_ZERO;
     }
-
+    /* skip to the first part with non-default imm16 values. */
     int i;
-    for(i = 0; i < 4; i++) {
-        if (parts[i].imm != default_val) break;
-    }
+    for(i = 0; i < 4; i++) if (parts[i].imm16 != default_val) break;
     uint8_t instr_bytes[4] = {0, 0, 0, 0};
+    /* check if the end was reached without finding a non-default value */
     if (i == 4) {
         /* all are the default value, so use this fallback instruction */
-        /* (MOVZ or MOVN) reg, default_val */
+        /* (MOVZ|MOVN) x.reg, default_val */
         arm64_mov(lead_mt, default_val, A64_SL_NO_SHIFT, reg, instr_bytes);
         if (!write_obj(fd, &instr_bytes, 4, sz)) return false;
     } else {
         /* at least one needs to be set */
-        /* (MOVZ or MOVN) reg, lead_imm << lead_shift */
-        arm64_mov(lead_mt, parts[i].imm, parts[i].shift, reg, instr_bytes);
+        /* (MOVZ|MOVN) x.reg, lead_imm{, lsl lead_shift} */
+        arm64_mov(lead_mt, parts[i].imm16, parts[i].shift, reg, instr_bytes);
         if (!write_obj(fd, &instr_bytes, 4, sz)) return false;
-        for(++i; i < 4; i++) if (parts[i].imm != default_val) {
-            /*  MOVK reg, imm16 << shift */
+        for(++i; i < 4; i++) if (parts[i].imm16 != default_val) {
+            /*  MOVK x[reg], imm16{, lsl shift} */
             arm64_mov(
                 A64_MT_KEEP,
-                parts[i].imm,
+                parts[i].imm16,
                 parts[i].shift,
                 reg,
                 instr_bytes
@@ -131,16 +146,18 @@ bool arm64_set_reg(uint8_t reg, int64_t imm, int fd, off_t *sz) {
     return true;
 }
 
+/* MOV x.dst, x.src
+ * technically an alias for ORR x.dst, XZR, x.src */
 bool arm64_reg_copy(uint8_t dst, uint8_t src, int fd, off_t *sz) {
-    /* MOV dst, src
-     * technically an allias for ORR dst, XZR, src */
     return write_obj(fd, (uint8_t[]){0xe0 | dst, 0x01, src, 0xaa}, 4, sz);
 }
 
+/* SVC 0 */
 bool arm64_syscall(int fd, off_t *sz) {
     return write_obj(fd, (uint8_t[]){0x01, 0x00, 0x00, 0xd4}, 4, sz);
 }
 
+/* NOP; NOP; NOP */
 bool arm64_nop_loop_open(int fd, off_t *sz) {
     uint8_t instr_bytes[12] = {
         0x1f, 0x20, 0x03, 0xd5, /* NOP */
@@ -150,6 +167,7 @@ bool arm64_nop_loop_open(int fd, off_t *sz) {
     return write_obj(fd, instr_bytes, 12, sz);
 }
 
+/* LDRB w.aux, x.reg; TST w.aux, 0xff; B.cond offset */
 static bool arm64_branch_cond(
     uint8_t reg, int64_t offset, int fd, off_t *sz, uint8_t cond
 ) {
@@ -171,9 +189,9 @@ static bool arm64_branch_cond(
     uint32_t offset_value = 1 + ((((uint32_t) offset) >> 2) & 0x7fffff);
     uint8_t aux = arm64_aux_reg(reg);
     uint8_t test_and_branch[12] = {
-        /* after arm64_inject_reg_operands, will be LDRB aux, [reg] */
+        /* after arm64_inject_reg_operands, will be LDRB w.aux, x.reg */
         0x00, 0x04, 0x40, 0x38,
-        /* TST reg, 0xff */
+        /* TST x.reg, 0xff */
         0x1f | aux << 5, (aux >> 3) | 0x1c, 0x40, 0xf2,
         /* B.cond offset */
         cond | (offset_value << 5), offset_value >> 3, offset_value >> 11, 0x54
@@ -182,11 +200,13 @@ static bool arm64_branch_cond(
     return write_obj(fd, test_and_branch, 12, sz);
 }
 
+/* LDRB w.aux, x.reg; TST w.aux, 0xff; B.NE offset */
 bool arm64_jump_not_zero(uint8_t reg, int64_t offset, int fd, off_t *sz) {
     /* 1 is the not zero / not equal condition code */
     return arm64_branch_cond(reg, offset, fd, sz, 1);
 }
 
+/* LDRB w.aux, x.reg; TST w.aux, 0xff; B.E offset */
 bool arm64_jump_zero(uint8_t reg, int64_t offset, int fd, off_t *sz) {
     /* 0 is the zero / equal condition code */
     return arm64_branch_cond(reg, offset, fd, sz, 0);
@@ -224,9 +244,9 @@ static bool arm64_add_sub(
         /* different byte values are needed than normal here */
         uint8_t op_byte = (op == A64_OP_ADD) ? 0x8b : 0xcb;
         uint8_t aux = arm64_aux_reg(reg);
-        /* set the auxiliary register to the target value */
+        /* set register x.aux to the target value */
         if (!arm64_set_reg(aux, (int64_t)imm, fd, sz)) return false;
-        /* either ADD reg, reg, aux or SUB reg, reg, aux */
+        /* either ADD x.reg, x.reg, x.aux or SUB x.reg, x.reg, x.aux */
         uint8_t instr_bytes[4] = { 0, 0, aux, op_byte };
         arm64_inject_reg_operands(reg, reg, instr_bytes);
         return write_obj(fd, &instr_bytes, 4, sz);
@@ -289,12 +309,13 @@ static bool arm64_add_sub_byte(
     uint8_t aux = arm64_aux_reg(reg);
     uint8_t instr_bytes[12] = {
         0x00, 0x00, 0x00, 0x00,
-        /* set middle instruction to ADD aux, aux, imm or SUB aux, aux, imm */
+        /* set middle instruction to (ADD|SUB) x.aux, x.aux, imm */
         aux | (aux << 5), (imm << 2) | (aux >> 3), imm >> 6, op,
         0x00, 0x00, 0x00, 0x00
     };
+    /* load the byte in address stored in x.reg into x.aux */
     arm64_load_from_byte(reg, aux, instr_bytes);
-    /* write the lowest byte in aux back to the address stored in reg */
+    /* store the lowest byte in x.aux back to the address in x.reg */
     arm64_store_to_byte(reg, aux, &(instr_bytes[8]));
     return write_obj(fd, &instr_bytes, 12, sz);
 }
