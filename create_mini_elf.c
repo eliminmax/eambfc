2e3c79cbe70f2d1f2dc23612ec2f55c9e96fbeee
Eli Array Minkoff <eli@planetminkoff.com>
2024-10-23 23:39:21 -0400

    improve create_mini_elf comments

--- create_mini_elf.c
+++ create_mini_elf.c
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: GPL-3.0-only
  *
  * A simple program that creates a minimal AMD x86_64 Linux ELF binary that
- * simply calls exit(0), to validate that a system can run Linux binaries. */
+ * simply calls exit(0), to validate that a system can run such binaries. */
 
 #include <stdio.h> /* fputs, stderr, tmpfile, fileno, fclose */
 #include <stdlib.h> /* EXIT_FAILURE, EXIT_SUCCESS */
@@ -15,18 +15,23 @@
 #include "compile.h" /* bf_compile */
 
 int main(void) {
+    /* a tmpfile will be open by default, so it's a portable way to have an
+     * empty source file. */
     FILE* tmp_file = tmpfile();
     if (tmp_file == NULL) {
         fputs("Failed to open tmpfile.\n", stderr);
         return EXIT_FAILURE;
     }
 
+    /* bf_compile expects a fd number rather than a file pointer */
     int in_fd = fileno(tmp_file);
     if (in_fd == -1) {
         fputs("Failed to get file descriptor for tmp_file.\n", stderr);
         fclose(tmp_file);
         return EXIT_FAILURE;
     }
+
+    /* compile an empty program called mini_elf */
     int out_fd = open("mini_elf", O_WRONLY | O_CREAT | O_TRUNC, 0755);
     if (out_fd == -1) {
         close(in_fd);
