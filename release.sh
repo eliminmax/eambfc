0dcf15fa4fcc382b91e397408ceee0c0e4eef887
Eli Array Minkoff <eli@planetminkoff.com>
2024-10-31 00:43:36 -0400

    Fix S390x target toggle
    Turns out that the character '0' appears in "S390X". Who knew?

--- release.sh
+++ release.sh
@@ -135,7 +135,7 @@ test_for_arch() {
 }
 
 # enable s390x target for testing purposes
-sed -i /TARGET_S390X/s/0/1/ config.h
+sed -i '/TARGET_S390X/s/ 0/ 1/' config.h
 # ensure strict and ubsan builds work at all gcc optimization levels
 for o_lvl in 0 1 2 3 s fast g z; do
     make -s CC=gcc CFLAGS="-O$o_lvl" clean ubsan strict eambfc;
