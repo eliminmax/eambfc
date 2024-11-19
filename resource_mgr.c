4dec0bfacde2b962bf42021350325af7fe87d3c0
Eli Array Minkoff <eli@planetminkoff.com>
2024-11-19 13:50:43 -0500

    Add resource_mgr code
    This code is intended to provide a "Resource Manager" to ensure that
    open files and unfreed mallocs are cleanly handled if program terminates
    early. They are currently unused

---
+++ resource_mgr.c
@@ -0,0 +1,172 @@
+/* SPDX-FileCopyrightText: 2024 Eli Array Minkoff
+ *
+ * SPDX-License-Identifier: GPL-3.0-only
+ * provides wrappers around various functions that need cleanup at the end of
+ * the process, and calls the cleanup functions as needed if the end was reached
+ * improperly */
+/* C99 */
+#include <stdlib.h> /* {m,re}alloc, free, atexit */
+#include <string.h> /* memmove */
+/* POSIX */
+#include <unistd.h> /* close */
+#include <fcntl.h> /* open */
+/* internal */
+#include "types.h" /* ifast_8, bool, mode_t, size_t */
+#include "err.h" /* internal_err, alloc_err */
+
+#define MAX_ALLOCS 64
+#define MAX_FDS 16
+
+/* with the maximum numbers of entries as low as they are, no fancy data
+ * structure needed. A struct is used just to keep all of the Resource Manager's
+ * internal state in one place. */
+static struct resource_tracker {
+    void *allocs[MAX_ALLOCS];
+    int fds[MAX_FDS];
+    /* index variables are for the NEXT entry in the array. */
+    ifast_8 alloc_i = 0;
+    ifast_8 fd_i = 0;
+} resources;
+
+static ifast_8 alloc_index(void *ptr) {
+    for (ifast_8 i = 0; i <= resources.alloc_i; i++) {
+        if (resources.allocs[i] == ptr) return i;
+    }
+    return -1;
+}
+
+void *mgr_malloc(size_t size) {
+    if (++resources.alloc_i > MAX_ALLOCS) {
+        internal_err(
+            "TOO_MANY_ALLOCS",
+            "Allocated too many times for resource_mgr to track."
+        );
+        /* will never return, as internal_err calls exit(EXIT_FAILURE) */
+        return NULL;
+    }
+
+    void* result = malloc(size);
+    if (result == NULL) {
+        /* don't skip over this array index */
+        resources.alloc_i--;
+        alloc_err();
+    } else {
+        resources.allocs[resources.alloc_i] = result;
+    }
+    return result;
+}
+
+void mgr_free(void *ptr) {
+    ifast_8 index = alloc_index(ptr);
+    if (index == -1) {
+        internal_err(
+            "MGR_FREE_UNKNOWN",
+            "mgr_free called with an unregistered *ptr value"
+        );
+        return;
+    }
+    size_t to_move = (index - resources.alloc_i) * sizeof(void*);
+    memmove(
+        &(resources.allocs[index]),
+        &(resources.allocss[index + 1]),
+        to_move
+    );
+}
+
+void *mgr_realloc(void *ptr, size_t size) {
+    ifast_8 index = alloc_index(ptr);
+    if (index == -1) {
+        internal_err(
+            "MGR_REALLOC_UNKNOWN",
+            "mgr_realloc called with an unregistered *ptr value"
+        );
+        return NULL;
+    }
+    void *new_ptr = realloc(ptr, size);
+    if (new_ptr == NULL) {
+        free(resources.allocs[index]);
+        alloc_err();
+        return NULL;
+    }
+    resources.allocs[index] = new_ptr;
+    return new_ptr;
+}
+
+static int mgr_open_handler(
+    const char *pathname, int flags, mode_t mode, bool with_mode
+) {
+    if (++resources.fd_i > MAX_OPENS) {
+        internal_err(
+            "TOO_MANY_OPENS",
+            "Opened too many files for resource_mgr to track."
+        );
+        /* will never return, as internal_err calls exit(EXIT_FAILURE) */
+        return -1;
+    }
+    int result;
+    if (with_mode) {
+        result = open(pathname, flags, mode);
+    } else {
+        result = open(pathname, flags);
+    }
+    if (result == -1) {
+        /* don't skip over this array index */
+        resources.fd_i--;
+    } else {
+        resources.fds[resources.fd_i] = result;
+    }
+    return result;
+}
+
+/* same semantics as open with a specified mode */
+int mgr_open_m(const char *pathname, int flags, mode_t mode) {
+    return mgr_open_handler(pathname, flags, mode, true);
+}
+
+/* same semantics as open without a specified mode */
+int mgr_open(const char *pathname, int flags) {
+    return mgr_open_handler(pathname, flags, 0, false);
+}
+
+int mgr_close(int fd) {
+    ifast_8 index = -1;
+    for (ifast_8 i = 0; i <= resources.fd_i; i++) {
+        if (resources.fds[i] == ptr) {
+            index = i;
+            break;
+        }
+    }
+    if (index == -1) {
+        internal_err(
+            "MGR_CLOSE_UNKNOWN",
+            "mgr_close called with an unregistered fd value"
+        );
+        /* will never return, as internal_err calls exit(EXIT_FAILURE) */
+        return -1;
+    }
+    /* remove fd from resources */
+    size_t to_move = (index - resources.fd_i) * sizeof(int);
+    memmove(&(resources.fds[index]), &(resources.fds[index + 1]), to_move);
+    resources.fd_i--;
+    return close(fd);
+}
+
+void cleanup(void) {
+    while (--resources.alloc_i > -1) free(resources.allocs[resources.alloc_i]);
+    while (--resources.fd_i > -1) close(resources.fds[resources.fd_i]);
+}
+
+void register_mgr(void) {
+    static bool registered = false;
+    /* if already registered, do nothing. */
+    if (registered) return;
+    /* atexit returns 0 on success, and a nonzero value otherwise. */
+    if (atexit(cleanup)) {
+        internal_err(
+            "MGR_ATEXIT_FAILED",
+            "Failed to register cleanup function with atexit"
+        );
+    } else {
+        registered = true;
+    }
+}
---
+++ resource_mgr.h
@@ -0,0 +1,92 @@
+/* SPDX-FileCopyrightText: 2024 Eli Array Minkoff
+ *
+ * SPDX-License-Identifier: GPL-3.0-only
+ *
+ * Provides an interface to a semi-opaque "Resource Manager" which wraps around
+ * `malloc`, `realloc`, `free`, `open`, and `close`, and tries to properly clean
+ * up in case of any sort of failure.
+ *
+ * Internal calls to malloc or realloc that fail are treated as fatal errors,
+ * but internal calls to open are not, as if memory allocation fails, then it's
+ * assumed that any future failures will also fail, and the program can't
+ * proceed, but if a file fails to open, it could be a permissions issue of some
+ * kind, and if multiple files are to be compiled, others can still be
+ * attempted.
+ *
+ * The "Resource Manager" registers an internal function with atexit to close
+ * any registered file descriptors and free any registered allocations, though
+ * that's intended as a fallback mechanism to be able to cleanly exit in case of
+ * unexpected state, so allocations and file descriptors shouldn't be left open
+ * just because that happens. */
+
+/* internal */
+#include "types.h" /* mode_t, size_t, bool */
+
+/* MUST BE CALLED to register the internal cleanup function with atexit.
+ *
+ * Fatal Error Calls:
+ * - if atexit failed to register Resource Manager cleanup function, calls
+ *   internal_err with error code `MGR_ATEXIT_FAILED` */
+void register_mgr(void);
+
+/* mgr_malloc has the same semantics as malloc, but registers allocations that
+ * it has returned, and any still registered will be freed when eambfc exits.
+ *
+ * Any allocation done with mgr_malloc must be 'realloc'ed and 'free'd with the
+ * mgr_realloc and mgr_free functions, to ensure that they are properly tracked
+ * and unregistered by the Resource Manager, otherwise it may try to free them
+ * again, resulting in Double Frees or other Undefined Behavior, or run out of
+ * space to register new allocations.
+ *
+ * Fatal Error Calls:
+ *  - if called with 64 allocations already registered, calls internal_err with
+ *    error code `TOO_MANY_ALLOCS`
+ *  - if internal call to malloc fails, calls alloc_err
+ *
+ * Because it calls alloc_err on failure, it never returns NULL. */
+void *mgr_malloc(size_t size);
+
+/* mgr_realloc has the same semantics as realloc, assuming it succeeds, but it
+ * exits on failure. Because of that, `p = mgr_realloc(p, size);` is not a bug.
+ *
+ * Fatal Error Calls:
+ *  - if ptr is not from a registered allocation, calls internal_err with error
+ *    code `MGR_REALLOC_UNKNOWN`
+ *  - if internal call to realloc fails, frees ptr then calls alloc_err
+ *
+ * Because it calls alloc_err on failure, it never returns NULL. */
+void *mgr_realloc(void *ptr, size_t size);
+
+/* mgr_free has the same semantics as free, but unregisters the tracked
+ * allocation.
+ *
+ * Fatal Error Calls:
+ * - if ptr is not from a registered allocation, it calls internal_err with
+ *   error code `MGR_FREE_UNKNOWN` */
+void mgr_free(void *ptr);
+
+/* mgr_open_m and mgr_open have the same semantics as a call to open with and
+ * without a mode specified respectively. Both register the file descriptors
+ * they return, and any still registered will be closed when eambfc exits.
+ *
+ * Any file descriptors opened with mgr_open or mgr_open_m must be closed with
+ * mgr_close, to ensure that they are properly unregistered by the Resource
+ * Manager, otherwise it may try to close them again, or run out of space to
+ * register new file descriptors.
+ *
+ * If internal call to open returns -1, they don't register the file descriptor.
+ *
+ * Fatal Error Calls:
+ *  - if called with 16 file descriptors already registered, call internal_err
+ *    with error code `TOO_MANY_OPENS`.
+*/
+int mgr_open_m(const char *pathname, int flags, mode_t mode);
+int mgr_open(const char *pathname, int flags);
+
+/* mgr_close has the same semantics as close, but unregisters the tracked file
+ * descriptor.
+ *
+ * Fatal Error Calls:
+ * - if fd is not a registered file descriptor, it calls internal_err with
+ *   error code `MGR_CLOSE_UNKNOWN` */
+int mgr_close(int fd);
