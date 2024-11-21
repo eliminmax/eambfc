b2b8edc758b19f034b2bc8c90c8e8981ec1d3430
Eli Array Minkoff <eli@planetminkoff.com>
2024-11-21 09:44:31 -0500

    Fix double-$ copied from Makefile

--- gen_version_h.sh
+++ gen_version_h.sh
@@ -1,10 +1,11 @@
 # SPDX-FileCopyrightText: 2024 Eli Array Minkoff
 #
 # SPDX-License-Identifier: GPL-3.0-only
+
 if command -v git >/dev/null && [ -e .git ]; then
-    git_str="$$(git log -n1 --pretty=format:'git commit: %h')";
-    if [ -n "$$(git status --short)" ]; then
-        git_str="$$git_str (with local changes)";
+    git_str="$(git log -n1 --pretty=format:'git commit: %h')";
+    if [ -n "$(git status --short)" ]; then
+        git_str="$git_str (with local changes)";
     fi
 else
     git_str='Not built from git repo';
