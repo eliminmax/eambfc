a1020b579e476f5d8b5d579018c6625d22b163ff
Eli Array Minkoff <eli@planetminkoff.com>
2025-02-06 15:08:24 -0500

    Document print_syscalls.c properly

--- print_syscalls.c
+++ print_syscalls.c
@@ -1,14 +1,18 @@
-/* SPDX-FileCopyrightText: 2023-2024 Eli Array Minkoff
+/* SPDX-FileCopyrightText: 2023-2025 Eli Array Minkoff
  *
  * SPDX-License-Identifier: 0BSD
  *
  * This program prints the system call numbers for the syscalls used within
  * the eambfc project. It was adapted from a similar tool created for the
- * tiny-clear-elf project (https://github.com/eliminmax/tiny-clear-elf) */
+ * tiny-clear-elf project (https://github.com/eliminmax/tiny-clear-elf)
+ *
+ * The intent is for it to be compiled for a new target architecture, then ran
+ * through qemu-binfmt, as a quick way to get the system call numbers for that
+ * architecture */
 
 /* C99 */
 #include <stdio.h>
-/* POSIX */
+/* GLIBC */
 #include <sys/syscall.h>
 
 int main(void) {
