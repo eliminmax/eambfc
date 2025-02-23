7649871265e9040a29219b96689bc4c80c057fe6
Eli Array Minkoff <eli@planetminkoff.com>
2025-02-23 18:39:30 -0500

    Port improvements to arm backend from eambfc-rs
    Since creating the riscv64 backend in Rust, I've reworked the arm
    version to streamline it, largely based on techniques used in the
    riscv64 backend. While some have to do with Rust's type system, other
    improvements could be ported over as-is, which this commit does.

--- backend_arm64.c
+++ backend_arm64.c
@@ -10,16 +10,10 @@
 #include "compat/elf.h" /* EM_X86_64, ELFDATA2LSB */
 #include "config.h" /* BFC_TARGET_ARM64 */
 #include "err.h" /* basic_err */
+#include "serialize.h"
 #include "types.h" /* [iu]{8,32,64}, bool, off_t, size_t, UINT64_C, sized_buf */
 #include "util.h" /* append_obj */
 #if BFC_TARGET_ARM64
-
-/* mark a series of bytes within a u8 array as being a single instruction,
- * mostly to prevent automated code formatting from splitting them up */
-#define INSTRUCTION(...) __VA_ARGS__
-
-/* padding bytes to be replaced with an instruction within a u8 array. */
-#define PAD_INSTRUCTION 0x00, 0x00, 0x00, 0x00
 
 /* in MOVK, MOVZ, and MOVN instructions, these correspond to the bits used
  * within the 3rd byte to indicate shift level. */
@@ -37,61 +31,31 @@ typedef enum { A64_OP_ADD = 0x91, A64_OP_SUB = 0xd1 } arith_op;
 /* the final byte of each of the MOVK, MOVN, and MOVZ instructions are used as
  * the corresponding enum values here */
 typedef enum {
-    A64_MT_KEEP = 0xf2,
-    A64_MT_ZERO = 0xd2,
-    A64_MT_INVERT = 0x92
+    A64_MT_KEEP = 0x3,
+    A64_MT_ZERO = 0x2,
+    A64_MT_INVERT = 0x0
 } mov_type;
 
-/* For an instruction that takes 2 registers, OR their bit values into the
- * appropriate parts of the machine code bytes in dst. */
-static void inject_reg_operands(u8 rt, u8 rn, u8 dst[4]) {
-    dst[0] |= (rt | rn << 5);
-    dst[1] |= (rn >> 3);
+/* set dst to the machine code for STRB w17, x.reg */
+static void store_to_byte(u8 src, u8 *dst) {
+    serialize32le(0x38000411 | (((u32)src) << 5), dst);
 }
 
-/* OR the immediate bytes for imm, shift, and x.reg into dst.
- *
- * This assumes that dst has everything except the register, shift, and
- * immediate bits set, and that the immediate bits are set to zero. */
-static void inject_imm16_operands(u16 imm, shift_lvl shift, u8 reg, u8 *dst) {
-    dst[0] |= (reg | ((imm & 07) << 5));
-    dst[1] |= (imm >> 3);
-    dst[2] |= (shift | (imm >> 11));
+/* set dst to the machine code for LDRB w17, x.reg */
+static void load_from_byte(u8 reg, u8 dst[4]) {
+    serialize32le(0x38400411 | (((u32)reg) << 5), dst);
 }
 
-/* set dst to the machine code for STRB w.aux, x.reg */
-static void store_to_byte(u8 reg, u8 aux, u8 dst[4]) {
-    dst[0] = 0x00;
-    dst[1] = 0x04;
-    dst[2] = 0x00;
-    dst[3] = 0x38;
-    inject_reg_operands(aux, reg, dst);
-}
-
-/* set dst to the machine code for LDRB w.aux, x.reg */
-static void load_from_byte(u8 reg, u8 aux, u8 dst[4]) {
-    dst[0] = 0x00;
-    dst[1] = 0x04;
-    dst[2] = 0x40;
-    dst[3] = 0x38;
-    inject_reg_operands(aux, reg, dst);
-}
-
-/* return an scratch register that isn't the same as x.reg to use */
-static u8 aux_reg(u8 reg) {
-    return (reg == 17) ? 16 : 17;
-}
+const u8 TEMP_REG = 17;
 
 /* set dst to the machine code for one of MOVK, MOVN, or MOVZ, depending on mt,
  * with the given operands. */
 static void mov(mov_type mt, u16 imm, shift_lvl shift, u8 reg, u8 *dst) {
     /* for MOVN, the bits need to be inverted. Ask Arm, not me. */
-    u16 imm_bits = (mt == A64_MT_INVERT) ? ~imm : imm;
-    dst[0] = 0x00;
-    dst[1] = 0x00;
-    dst[2] = 0x80;
-    dst[3] = mt;
-    inject_imm16_operands(imm_bits, shift, reg, dst);
+    if (mt == A64_MT_INVERT) imm = ~imm;
+    u32 instr = 0x92800000 | ((u32)mt << 29) | ((u32)shift << 16) |
+                ((u32)imm << 5) | reg;
+    serialize32le(instr, dst);
 }
 
 /* Choose a combination of MOVZ, MOVK, and MOVN that sets register x.reg to
@@ -123,7 +87,7 @@ static bool set_reg(u8 reg, i64 imm, sized_buf *dst_buf) {
     int i;
     for (i = 0; i < 4; i++)
         if (parts[i].imm16 != default_val) break;
-    u8 instr_bytes[4] = {0, 0, 0, 0};
+    u8 instr_bytes[4];
     /* check if the end was reached without finding a non-default value */
     if (i == 4) {
         /* all are the default value, so use this fallback instruction */
@@ -160,15 +124,13 @@ static bool syscall(sized_buf *dst_buf) {
     return append_obj(dst_buf, (u8[]){0x01, 0x00, 0x00, 0xd4}, 4);
 }
 
-/* NOP; NOP; NOP */
-#define NOP 0x1f, 0x20, 0x03, 0xdf
-
 static bool nop_loop_open(sized_buf *dst_buf) {
-    u8 instr_bytes[12] = {NOP, NOP, NOP};
-    return append_obj(dst_buf, &instr_bytes, 12);
+#define NOP 0x1f, 0x20, 0x03, 0xdf
+    return append_obj(dst_buf, (u8[]){NOP, NOP, NOP}, 12);
+#undef NOP
 }
 
-/* LDRB w.aux, x.reg; TST w.aux, 0xff; B.cond offset */
+/* LDRB w17, x.reg; TST w17, 0xff; B.cond offset */
 static bool branch_cond(u8 reg, i64 offset, sized_buf *dst_buf, u8 cond) {
     if ((offset % 4) != 0) {
         basic_err(
@@ -181,34 +143,31 @@ static bool branch_cond(u8 reg, i64 offset, sized_buf *dst_buf, u8 cond) {
      * (19 immediate bits are used, but as it must be a multiple of 4, it treats
      * them as though they're followed by an implicit 0b00, so it needs to fit
      * within the range of possible 21-bit 2's complement values. */
-    if ((offset < -0x200000 || offset > 0x1fffff)) {
+    if (!bit_fits(offset, 21)) {
         basic_err(
             "JUMP_TOO_LONG",
             "offset is outside the range of possible 21-bit signed values"
         );
         return false;
     }
-    u32 off_val = 1 + ((((u32)offset) >> 2) & 0x7fffff);
-    u8 aux = aux_reg(reg);
-    u8 test_and_branch[12] = {
-        /* after inject_reg_operands, will be LDRB w.aux, x.reg */
-        PAD_INSTRUCTION,
-        /* TST x.reg, 0xff */
-        INSTRUCTION(0x1f | aux << 5, (aux >> 3) | 0x1c, 0x40, 0xf2),
-        /* B.cond offset */
-        INSTRUCTION(cond | (off_val << 5), off_val >> 3, off_val >> 11, 0x54),
-    };
-    load_from_byte(reg, aux, test_and_branch);
+    u32 off_val = ((offset >> 2) + 1) & 0x7ffff;
+    u8 test_and_branch[12];
+    /* LDRB w17, x.reg */
+    load_from_byte(reg, test_and_branch);
+    /* TST x17, 0xff */
+    serialize32le(0xf2401e3f, &test_and_branch[4]);
+    /* B.cond offset */
+    serialize32le(0x54000000 | cond | (off_val << 5), &test_and_branch[8]);
     return append_obj(dst_buf, test_and_branch, 12);
 }
 
-/* LDRB w.aux, x.reg; TST w.aux, 0xff; B.NE offset */
+/* LDRB w17, x.reg; TST w17, 0xff; B.NE offset */
 static bool jump_not_zero(u8 reg, i64 offset, sized_buf *dst_buf) {
     /* 1 is the not zero / not equal condition code */
     return branch_cond(reg, offset, dst_buf, 1);
 }
 
-/* LDRB w.aux, x.reg; TST w.aux, 0xff; B.E offset */
+/* LDRB w17, x.reg; TST w17, 0xff; B.E offset */
 static bool jump_zero(u8 reg, i64 offset, sized_buf *dst_buf) {
     /* 0 is the zero / equal condition code */
     return branch_cond(reg, offset, dst_buf, 0);
@@ -223,16 +182,16 @@ static bool add_sub_imm(
         basic_err("IMMEDIATE_TOO_LARGE", "value is invalid for shift level.");
         return false;
     }
-    /* if shift is set to true, imm needs to be shifted back by 12 bits */
-    u16 imm_bits = shift ? (imm >> 12) : imm;
+    /* align the immediate bits */
+    u32 aligned = shift ? (imm >> 2) : (imm << 10);
     /* (ADD|SUB) x.reg, x.reg, imm{, lsl12} */
-    u8 instr_bytes[4] = {
-        reg | (reg << 5),
-        (reg >> 3) | ((imm_bits << 2) & 0xff),
-        (imm_bits >> 6) | (shift ? 0x40 : 0x0),
-        op,
-    };
-    return append_obj(dst_buf, &instr_bytes, 4);
+    u8 i_bytes[4];
+    serialize32le(
+        (op << 24) | aligned | (shift ? (1 << 22) : 0) | (reg << 5) | reg,
+        i_bytes
+    );
+
+    return append_obj(dst_buf, &i_bytes, 4);
 }
 
 static bool add_sub(u8 reg, arith_op op, u64 imm, sized_buf *dst_buf) {
@@ -241,9 +200,8 @@ static bool add_sub(u8 reg, arith_op op, u64 imm, sized_buf *dst_buf) {
      * and shift the higher 12 of the 24 bits. If the 12 lower bits are
      * non-zero, then also ADD or SUB them afterwards.
      *
-     * If the immediate does not fit within the lower 24 bits, then first set an
-     * auxiliary register to the immediate, then ADD or SUB that, assuming that
-     * it still fits within the 64-bit signed limit. */
+     * If the immediate does not fit within the lower 24 bits, then first set
+     * x17 to the immediate, then ADD or SUB it. */
     if (imm < UINT64_C(0x1000)) {
         return add_sub_imm(reg, imm, false, op, dst_buf);
     } else if (imm < UINT64_C(0x1000000)) {
@@ -255,12 +213,13 @@ static bool add_sub(u8 reg, arith_op op, u64 imm, sized_buf *dst_buf) {
     } else if (imm < UINT64_C(0x7fffffffffffffff)) {
         /* different byte values are needed than normal here */
         u8 op_byte = (op == A64_OP_ADD) ? 0x8b : 0xcb;
-        u8 aux = aux_reg(reg);
-        /* set register x.aux to the target value */
-        if (!set_reg(aux, (i64)imm, dst_buf)) return false;
-        /* either ADD x.reg, x.reg, x.aux or SUB x.reg, x.reg, x.aux */
-        u8 instr_bytes[4] = {0, 0, aux, op_byte};
-        inject_reg_operands(reg, reg, instr_bytes);
+        /* set register x17 to the target value */
+        if (!set_reg(TEMP_REG, (i64)imm, dst_buf)) return false;
+        /* either ADD x.reg, x.reg, x17 or SUB x.reg, x.reg, x17 */
+        u8 instr_bytes[4];
+        serialize32le(
+            op_byte << 24 | TEMP_REG << 16 | reg | reg << 5, instr_bytes
+        );
         return append_obj(dst_buf, &instr_bytes, 4);
     }
     /* over the 64-bit signed int limit, so print an error and return false. */
@@ -291,20 +250,18 @@ static bool dec_reg(u8 reg, sized_buf *dst_buf) {
     return add_sub(reg, A64_OP_SUB, 1, dst_buf);
 }
 
-/* to increment or decrement a byte, first load it into an auxiliary register,
- * then call an inner function (either inc_reg or dec_reg) on that register,
- * and finally, store the least significant byte of the register into that
- * memory address. */
-static bool inc_dec_byte(
-    u8 reg, sized_buf *dst_buf, bool (*inner_fn)(u8 reg, sized_buf *dst_buf)
-) {
-    u8 instr_bytes[4] = {0x00, 0x00, 0x00, 0x00};
-    u8 aux = aux_reg(reg);
-    load_from_byte(reg, aux, instr_bytes);
-    if (!append_obj(dst_buf, &instr_bytes, 4)) return false;
-    if (!inner_fn(aux, dst_buf)) return false;
-    store_to_byte(reg, aux, instr_bytes);
-    return append_obj(dst_buf, &instr_bytes, 4);
+/* to increment or decrement a byte, first load it into TEMP_REG,
+ * then call an inner function (either inc_reg or dec_reg) on TEMP_REG,
+ * and finally, store the least significant byte of TEMP_REG into the byte */
+typedef bool (*inc_dec_fn)(u8 reg, sized_buf *dst_buf);
+
+static bool inc_dec_byte(u8 reg, sized_buf *dst_buf, inc_dec_fn inner_fn) {
+    u8 i_bytes[4];
+    load_from_byte(reg, i_bytes);
+    if (!append_obj(dst_buf, &i_bytes, 4)) return false;
+    if (!inner_fn(TEMP_REG, dst_buf)) return false;
+    store_to_byte(reg, i_bytes);
+    return append_obj(dst_buf, &i_bytes, 4);
 }
 
 /* next, some thin wrappers around the inc_dec_byte function that can be used in
@@ -320,18 +277,21 @@ static bool dec_byte(u8 reg, sized_buf *dst_buf) {
 /* similar to add_sub_reg, but operating on an auxiliary register, after loading
  * from byte and before restoring to that byte, much like inc_dec_byte */
 static bool add_sub_byte(u8 reg, u8 imm8, arith_op op, sized_buf *dst_buf) {
-    u8 aux = aux_reg(reg);
-    u8 instr_bytes[12] = {
-        PAD_INSTRUCTION,
-        /* set middle instruction to (ADD|SUB) x.aux, x.aux, imm */
-        INSTRUCTION(aux | (aux << 5), (imm8 << 2) | (aux >> 3), imm8 >> 6, op),
-        PAD_INSTRUCTION,
-    };
-    /* load the byte in address stored in x.reg into x.aux */
-    load_from_byte(reg, aux, instr_bytes);
-    /* store the lowest byte in x.aux back to the address in x.reg */
-    store_to_byte(reg, aux, &(instr_bytes[8]));
-    return append_obj(dst_buf, &instr_bytes, 12);
+    u8 i_bytes[12];
+    /* load the byte in address stored in x.reg into x17 */
+    load_from_byte(reg, i_bytes);
+    /* (ADD|SUB) x.17, x.17, imm8 */
+    serialize32le((op << 24) | (imm8 << 10) | 0x231, &i_bytes[4]);
+    /* store the lowest byte in x17 back to the address in x.reg */
+    store_to_byte(reg, &i_bytes[8]);
+    return append_obj(dst_buf, i_bytes, 12);
+}
+
+/* a function to zero out a memory address. */
+static bool zero_byte(u8 reg, sized_buf *dst_buf) {
+    u8 i_bytes[4];
+    serialize32le(0x3800041f | reg << 5, i_bytes);
+    return append_obj(dst_buf, &i_bytes, 4);
 }
 
 /* now, the last few thin wrapper functions */
@@ -342,16 +302,6 @@ static bool add_byte(u8 reg, u8 imm8, sized_buf *dst_buf) {
 
 static bool sub_byte(u8 reg, u8 imm8, sized_buf *dst_buf) {
     return add_sub_byte(reg, imm8, A64_OP_SUB, dst_buf);
-}
-
-/* a function to zero out a memory address. It sets an auxiliary register to 0,
- * then stores its least significant byte in the address in reg. */
-static bool zero_byte(u8 reg, sized_buf *dst_buf) {
-    u8 aux = aux_reg(reg);
-    if (!set_reg(aux, 0, dst_buf)) return false;
-    u8 instr_bytes[4];
-    store_to_byte(reg, aux, instr_bytes);
-    return append_obj(dst_buf, &instr_bytes, 4);
 }
 
 static const arch_funcs FUNCS = {
