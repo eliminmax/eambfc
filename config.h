a688ac4b5a88f26a7bcd932a793bd803e1d7d584
Eli Array Minkoff <eli@planetminkoff.com>
2025-02-24 13:07:33 -0500

    Enable overriding of config.h

--- config.h
+++ config.h
@@ -11,17 +11,29 @@
 
 /* whether or not to support GNU argument parsing - requires _GNU_SOURCE to be
  * defined if enabled */
+#ifndef BFC_GNU_ARGS
 #define BFC_GNU_ARGS 0
+#endif /* BFC_GNU_ARGS */
 
 /* __BACKENDS__ when adding a backend, add a macro here */
 /* set to 0 if you don't want it included in the eambfc build. */
+#ifndef BFC_TARGET_ARM64
 #define BFC_TARGET_ARM64 1
+#endif /* BFC_TARGET_ARM64 */
+#ifndef BFC_TARGET_RISCV64
 #define BFC_TARGET_RISCV64 1
+#endif /* BFC_TARGET_RISCV64 */
+#ifndef BFC_TARGET_S390X
 #define BFC_TARGET_S390X 1
+#endif /* BFC_TARGET_S390X */
+#ifndef BFC_TARGET_X86_64
 #define BFC_TARGET_X86_64 1
+#endif /* BFC_TARGET_X86_64 */
 /* The target architecture - should be the same as the ELF e_machine value for
  * that architecture for consistency's sake. */
+#ifndef BFC_DEFAULT_TARGET
 #define BFC_DEFAULT_TARGET EM_X86_64
+#endif /* BFC_DEFAULT_TARGET */
 
 /* runs some preprocessor validation that the above settings are sane, and
  * defines some macros based on the configured default value */
