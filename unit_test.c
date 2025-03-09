bbc246a8136d9fa4755c5414d88fa90de5d1c884
Eli Array Minkoff <eli@planetminkoff.com>
2025-03-08 19:43:31 -0500

    Add unit tests for serialize functions

--- unit_test.c
+++ unit_test.c
@@ -8,13 +8,16 @@
 #include <CUnit/Basic.h>
 #include <CUnit/CUnit.h>
 CU_pSuite register_util_tests(void);
+CU_pSuite register_serialize_tests(void);
 
 int main(void) {
     if (CU_initialize_registry() != CUE_SUCCESS ||
-        register_util_tests() == NULL) {
+        register_util_tests() == NULL || register_serialize_tests() == NULL) {
         return CU_get_error();
     }
+
     /* Run all tests using the console interface */
+    CU_basic_set_mode(CU_BRM_VERBOSE);
     CU_basic_run_tests();
     CU_cleanup_registry();
     return CU_get_error();
