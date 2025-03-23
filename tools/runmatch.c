4d9b2cd4bbe45a48d7ad4fb2c2436b8375319327
Eli Array Minkoff <eli@planetminkoff.com>
2025-03-22 23:10:15 -0400

    Only fail if child args would overflow

--- tools/runmatch.c
+++ tools/runmatch.c
@@ -10,6 +10,7 @@
  * pass the command and flags to run, followed by the files to run the glob
  * against, the remaining args, separated by "{-}", as it's not meaningful to
  * any of the commands this tool is needed for */
+
 /* stdlib */
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,27 +19,29 @@
 #include <fnmatch.h>
 #include <unistd.h>
 
+#define MAX_ARGS 64
+
 int main(int argc, char *argv[]) {
+    char *chld_args[MAX_ARGS + 1] = {0};
     if (argc < 2) {
         fputs("Not enough arguments\n", stderr);
         return EXIT_FAILURE;
     }
 
-    if (argc > 64) {
-        fputs("Too many arguments\n", stderr);
-        return EXIT_FAILURE;
-    }
-
-    char *chld_args[64] = {0};
     int chld_argc = 0;
     char matched = 0;
     char split = 0;
     char *pat = argv[1];
     int argi;
+
     for (argi = 2; argi < argc; argi++) {
         if (strcmp(argv[argi], "{-}") == 0) {
             split = 1;
             break;
+        }
+        if (chld_argc == MAX_ARGS) {
+            fputs("Too many arguments to pass to child\n", stderr);
+            return EXIT_FAILURE;
         }
         chld_args[chld_argc++] = argv[argi];
     }
@@ -51,6 +54,10 @@ int main(int argc, char *argv[]) {
     for (argi++; argi < argc; argi++) {
         switch (fnmatch(pat, argv[argi], 0)) {
         case 0:
+            if (chld_argc == MAX_ARGS) {
+                fputs("Too many arguments to pass to child\n", stderr);
+                return EXIT_FAILURE;
+            }
             matched = 1;
             chld_args[chld_argc++] = argv[argi];
             break;
