251d8dd6abdb3f087dd9715f3926403c3eeadf89
Eli Array Minkoff <eli@planetminkoff.com>
2025-03-26 23:27:02 -0400

    Rework attributes.h

--- attributes.h
+++ attributes.h
@@ -4,15 +4,22 @@
  *
  * Macros which optionally resolve to attributes if supported, to enable better
  * static analysis and/or optimization */
-
 #ifndef BFC_ATTRIBUTES
 #define BFC_ATTRIBUTES 1
+
+#if defined __GNUC__ && defined __has_attribute
+#define HAS_GCC_ATTR(attr) __has_attribute(attr)
+#else /* defined __GNUC__ && defined __has_attribute */
+#define HAS_GCC_ATTR(attr) 0
+#endif /* defined __GNUC__ && defined __has_attribute */
+
 #define noreturn
 #define nonnull_args
 #define nonnull_arg(...)
 #define nonnull_ret
 #define const_fn
 #define malloc_like
+#define must_use
 
 #if __STDC_VERSION__ == 202311L
 #undef noreturn
@@ -20,30 +27,39 @@
 
 #elif __STDC_VERSION__ == 201710L || __STDC_VERSION__ == 201112L
 #undef noreturn
-#define noreturn _Noreturn
+#include <stdnoreturn.h>
 
-#elif defined(__GNUC__)
+#elif HAS_GCC_ATTR(__noreturn__)
 #undef noreturn
-#define noreturn __attribute__((noreturn))
-
+#define noreturn __attribute__((__noreturn__))
 #endif /* STDC_VERSION */
 
-#ifdef __GNUC__
+#if HAS_GCC_ATTR(__const__)
+#undef const_fn
+#define const_fn __attribute__((__const__))
+#endif /* HAS_GCC_ATTR(__const__) */
 
+#if HAS_GCC_ATTR(__malloc__)
+#undef malloc_like
+#define malloc_like __attribute__((__malloc__))
+#endif /* HAS_GCC_ATTR(__malloc__) */
+
+#if HAS_GCC_ATTR(__nonnull__)
 #undef nonnull_args
-#define nonnull_args __attribute__((nonnull))
-
+#define nonnull_args __attribute__((__nonnull__))
 #undef nonnull_arg
 #define nonnull_arg(...) __attribute__((nonnull(__VA_ARGS__)))
+#endif /* HAS_GCC_ATTR(__nonnull__) */
 
+#if HAS_GCC_ATTR(__returns_nonnull__)
 #undef nonnull_ret
-#define nonnull_ret __attribute__((returns_nonnull))
+#define nonnull_ret __attribute__((__returns_nonnull__))
+#endif /* HAS_GCC_ATTR(__returns_nonnull__) */
 
-#undef const_fn
-#define const_fn __attribute__((const))
+#if HAS_GCC_ATTR(__warn_unused_result__)
+#undef must_use
+#define must_use __attribute__((__warn_unused_result__))
+#endif /* HAS_GCC_ATTR(__warn_unused_result__) */
 
-#undef malloc_like
-#define malloc_like __attribute__((malloc))
-
-#endif /* __GNUC__ */
+#undef HAS_GCC_ATTR
 #endif /* BFC_NOATTRIBUTES */
