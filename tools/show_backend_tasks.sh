85b2c79c684dab7bec646e1fed483b937fd69704
Eli Array Minkoff <eli@planetminkoff.com>
2025-04-24 22:29:53 -0400

    Remove `exec` from show_backend_tasks.sh
    There's no need to `exec` over the new process.

--- tools/show_backend_tasks.sh
+++ tools/show_backend_tasks.sh
@@ -5,4 +5,4 @@
 # SPDX-License-Identifier: 0BSD
 
 # use [_] so that the pattern doesn't match itself
-cd "$(git rev-parse --show-toplevel)" && exec git grep -A3 '[_]_BACKENDS_[_]'
+cd "$(git rev-parse --show-toplevel)" && git grep -A3 '[_]_BACKENDS_[_]'
