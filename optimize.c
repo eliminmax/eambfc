5eef69252e5ed41388cab5ae348d041ba1d9d965
Eli Array Minkoff <eli@planetminkoff.com>
2025-05-10 20:36:23 -0400

    Create new optimization plumbing

--- optimize.c
+++ optimize.c
@@ -6,6 +6,7 @@
  * and replaces `[-]` and `[+]` sequences with `@`. */
 
 /* C99 */
+#include <stdlib.h>
 #include <string.h>
 /* internal */
 #include <attributes.h>
@@ -14,6 +15,298 @@
 #include "err.h"
 #include "optimize.h"
 #include "util.h"
+
+static nonnull_args InstrSeq *into_sequences(
+    const char *restrict code, size_t code_bytes, size_t *restrict seq_count
+) {
+    SizedBuf out_buf = (SizedBuf){NULL, 0, 0};
+    SrcLoc location = {.line = 1, .col = 0};
+    *seq_count = 0;
+
+    InstrSeq *current_instr;
+    char prev_raw = '\0';
+
+    /* handle the boilerplate of incrementing (*seq_count), setting prev_raw,
+     * and appending a new instruction with the provided TAG_VALUE */
+#define APPEND_INSTR(RAW_BYTE, TAG_VAL) \
+    ++(*seq_count); \
+    prev_raw = RAW_BYTE; \
+    current_instr = sb_reserve(&out_buf, sizeof(InstrSeq)); \
+    current_instr->source = (struct loc_info){location, i, i}; \
+    current_instr->tag = TAG_VAL
+
+    for (size_t i = 0; i < code_bytes; ++i) {
+        /* if it's not a UTF-8 continuation byte, increment the column number */
+        if ((code[i] & 0xc0) != 0x80) location.col++;
+
+        switch (code[i]) {
+            case '.':
+                APPEND_INSTR('.', ISEQ_WRITE);
+                break;
+            case ',':
+                APPEND_INSTR(',', ISEQ_READ);
+                break;
+            case '[':
+                APPEND_INSTR('[', ISEQ_LOOP_OPEN);
+                break;
+            case ']':
+                APPEND_INSTR(']', ISEQ_LOOP_CLOSE);
+                break;
+            case '+':
+                if (prev_raw == '+') {
+                    current_instr->source.end = i;
+                    current_instr->count++;
+                    current_instr->count &= 0xff;
+                    if (!current_instr->count) {
+                        prev_raw = '\0';
+                        out_buf.sz -= sizeof(InstrSeq);
+                        --(*seq_count);
+                    }
+                } else {
+                    APPEND_INSTR('+', ISEQ_ADD);
+                    current_instr->count = 1;
+                }
+                break;
+            case '>':
+                if (prev_raw == '>') {
+                    current_instr->source.end = i;
+                    current_instr->count++;
+                    if (!current_instr->count) {
+                        prev_raw = '\0';
+                        out_buf.sz -= sizeof(InstrSeq);
+                        --(*seq_count);
+                    }
+                } else {
+                    APPEND_INSTR('>', ISEQ_MOVE_RIGHT);
+                    current_instr->count = 1;
+                }
+                break;
+                /* cheating a bit by using ISEQ_ADD and ISEQ_MOVE_RIGHT with
+                 * negative counts cast to unsigned instead of ISEQ_SUB and
+                 * ISEQ_MOVE_LEFT with positive counts. The last step these are
+                 * returned to functions outside of this translation unit is to
+                 * convert them to the proper instructions. */
+            case '-':
+                if (prev_raw == '+') {
+                    current_instr->source.end = i;
+                    current_instr->count--;
+                    current_instr->count &= 0xff;
+                    if (!current_instr->count) {
+                        prev_raw = '\0';
+                        out_buf.sz -= sizeof(InstrSeq);
+                        --(*seq_count);
+                    }
+                } else {
+                    APPEND_INSTR('+', ISEQ_ADD);
+                    current_instr->count = -1;
+                }
+                break;
+            case '<':
+                if (prev_raw == '>') {
+                    current_instr->source.end = i;
+                    current_instr->count--;
+                    if (!current_instr->count) {
+                        prev_raw = '\0';
+                        out_buf.sz -= sizeof(InstrSeq);
+                        --(*seq_count);
+                    }
+                } else {
+                    APPEND_INSTR('>', ISEQ_MOVE_RIGHT);
+                    current_instr->count = -1;
+                }
+                break;
+            case '\n':
+                location.col = 0;
+                location.line++;
+                break;
+            default:
+                break;
+        }
+    }
+
+    return checked_realloc(out_buf.buf, out_buf.sz * sizeof(InstrSeq));
+}
+
+/* drop instruction sequences from `start` (inclusive) to `end` (exclusive),
+ * updating the value of `*len` accordingly */
+static nonnull_args void drop_instrs(
+    InstrSeq *seq, size_t *len, size_t start, size_t end
+) {
+    memmove(&seq[start], &seq[end], (*len - end) * sizeof(InstrSeq));
+    *len -= end - start;
+}
+
+/* check if any more instruction mergers can occur after the previous removal */
+static nonnull_args void recheck_mergable(
+    InstrSeq *seq, size_t index, size_t *len
+) {
+    if (index + 1 == *len) return;
+
+    while ((seq[index].tag == ISEQ_ADD || seq[index].tag == ISEQ_MOVE_RIGHT) &&
+           seq[index].tag == seq[index + 1].tag) {
+        switch (seq[index].tag) {
+            case ISEQ_ADD:
+            case ISEQ_SUB:
+                /* check if the tags are the same. If so, simply add their
+                 * values, otherwise, subtract the following value. */
+                if (seq[index].tag == seq[index + 1].tag) {
+                    seq[index].count += seq[index + 1].count;
+                } else {
+                    seq[index].count -= seq[index + 1].count;
+                }
+                seq[index].count &= 0xff;
+                break;
+            case ISEQ_MOVE_RIGHT:
+            case ISEQ_MOVE_LEFT:
+                /* check if the tags are the same. If so, simply add their
+                 * values, otherwise, subtract the following value. */
+                if (seq[index].tag == seq[index + 1].tag) {
+                    seq[index].count += seq[index + 1].count;
+                } else {
+                    seq[index].count -= seq[index + 1].count;
+                }
+                break;
+            default:
+                return;
+        }
+        if (seq[index].count) {
+            /* drop the merged instruction */
+            drop_instrs(seq, len, index, index + 1);
+        } else {
+            drop_instrs(seq, len, index, index + 2);
+            /* check if that lets the previous instruction merge */
+            if (index) --index;
+        }
+    }
+}
+
+#undef MERGE_IMPL
+
+static nonnull_args bool drain_loop(
+    InstrSeq *seq, size_t start, size_t *len, BFCError *err
+) {
+    size_t nest_level = 1;
+    for (size_t i = start + 1; i < *len; ++i) {
+        if (seq[i].tag == ISEQ_LOOP_OPEN) {
+            ++nest_level;
+        } else if (seq[i].tag == ISEQ_LOOP_CLOSE) {
+            --nest_level;
+            if (!nest_level) {
+                drop_instrs(seq, len, start, i + 1);
+                recheck_mergable(seq, start ? start - 1 : 0, len);
+                return true;
+            }
+        }
+    }
+    free(seq);
+    *err = (BFCError){
+        .id = BF_ERR_UNMATCHED_OPEN,
+        .msg.ref = "Could not optimize due to unmatched loop open",
+        .location = seq[start].source.location,
+        .has_location = true,
+    };
+    return false;
+}
+
+/* find and remove loops that can be determined to always start with `0` already
+ * in the current cell (meaning that they'll never run */
+enum loop_removal_result {
+    SUCCESS_CHANGED = 0,
+    SUCCESS_UNCHANGED = 1,
+    FAIL_UNMATCHED_OPEN = -1,
+};
+
+static nonnull_args enum loop_removal_result drop_dead_loops(
+    InstrSeq *sequence, size_t *len, BFCError *err
+) {
+    bool may_have_nonzero = true;
+    bool can_elim = true;
+    bool changed = false;
+    for (size_t i = 0; i < *len; ++i) {
+        while (can_elim && sequence[i].tag == ISEQ_LOOP_OPEN) {
+            changed = true;
+            if (!drain_loop(sequence, i, len, err)) return FAIL_UNMATCHED_OPEN;
+        }
+        switch (sequence[i].tag) {
+            case ISEQ_READ:
+            case ISEQ_ADD:
+            case ISEQ_SUB:
+                may_have_nonzero = false;
+                break;
+            default:
+                break;
+        }
+
+        can_elim = may_have_nonzero && sequence[i].tag == ISEQ_LOOP_CLOSE;
+    }
+    return changed ? SUCCESS_CHANGED : SUCCESS_UNCHANGED;
+}
+
+static nonnull_args void join_set_cells(InstrSeq *instructions, size_t *len) {
+    for (size_t i = 0; i + 2 < *len; ++i) {
+        if (instructions[i].tag == ISEQ_LOOP_OPEN &&
+            instructions[i + 1].tag == ISEQ_ADD &&
+            instructions[i + 1].count % 2 &&
+            instructions[i + 2].tag == ISEQ_LOOP_CLOSE) {
+            drop_instrs(instructions, len, i + 1, i + 3);
+            instructions[i].tag = ISEQ_SET_CELL;
+            if (i + 1 < *len && instructions[i + 1].tag == ISEQ_ADD) {
+                instructions[i].count = instructions[i + 1].count;
+                drop_instrs(instructions, len, i + 1, i + 2);
+            }
+        }
+    }
+}
+
+nonnull_args bool optimize_instructions(
+    const char *restrict code, size_t size, union opt_result *restrict result
+) {
+    result->output.instrs = into_sequences(code, size, &result->output.len);
+
+    enum loop_removal_result res;
+    while (
+        (res = drop_dead_loops(
+             result->output.instrs, &result->output.len, result->err
+         ))
+    ) {
+        if (res == FAIL_UNMATCHED_OPEN) return false;
+    }
+    join_set_cells(result->output.instrs, &result->output.len);
+    for (size_t i = result->output.len; i; --i) {
+        if (result->output.instrs[i - 1].tag == ISEQ_LOOP_CLOSE ||
+            result->output.instrs[i - 1].tag == ISEQ_READ ||
+            result->output.instrs[i - 1].tag == ISEQ_WRITE) {
+            break;
+        }
+        --result->output.len;
+    }
+    for (size_t i = 0; i < result->output.len; ++i) {
+        switch (result->output.instrs[i].tag) {
+            case ISEQ_ADD: {
+                i8 signed_count =
+                    sign_extend(result->output.instrs[i].count, 8);
+                if (signed_count < 0) {
+                    result->output.instrs[i].count = -signed_count;
+                    result->output.instrs[i].tag = ISEQ_SUB;
+                }
+            } break;
+            case ISEQ_MOVE_RIGHT: {
+                /* because `i64` is technically `int_least64_t`, which is
+                 * possibly more than 64 bits, this check still requires sign
+                 * extension */
+                i64 signed_count =
+                    sign_extend(result->output.instrs[i].count, 64);
+                if (signed_count < 0) {
+                    result->output.instrs[i].count = -signed_count;
+                    result->output.instrs[i].tag = ISEQ_MOVE_LEFT;
+                }
+            } break;
+            default:
+                break;
+        }
+    }
+    return true;
+}
 
 /* filter out the non-bf characters from code->buf */
 static nonnull_args void filter_non_bf(SizedBuf *code) {
@@ -207,7 +500,7 @@ nonnull_args bool filter_dead(SizedBuf *bf_code, const char *in_name) {
 
 static void strip_dead_code_test(void) {
     /* fill filterable with have a bunch of valid brainfuck code which does the
-     * equivalent of ">[->+<]", and ensure that it is optimized down to just
+     * equivalent of ">+[->+<]", and ensure that it is optimized down to just
      * that sequence. */
     SizedBuf filterable = newbuf(574);
     append_obj(
@@ -215,7 +508,7 @@ static void strip_dead_code_test(void) {
     );
     char *tgt = sb_reserve(&filterable, 256);
     memset(tgt, '+', 256);
-    append_obj(&filterable, "[+-]>", 5);
+    append_obj(&filterable, "[+-]>+", 5);
     tgt = sb_reserve(&filterable, 256);
     memset(tgt, '-', 256);
     append_obj(&filterable, "[->+<][,.]", 10);
@@ -239,7 +532,7 @@ static void strip_dead_code_test(void) {
     /* null-terminate before string comparison */
     tgt = sb_reserve(&filterable, 1);
     *tgt = '\0';
-    CU_ASSERT_STRING_EQUAL(filterable.buf, ">[->+<]");
+    CU_ASSERT_STRING_EQUAL(filterable.buf, ">+[->+<]");
     free(filterable.buf);
 }
 
--- optimize.h
+++ optimize.h
@@ -10,6 +10,68 @@
 #include <attributes.h>
 #include <types.h>
 
+#include "err.h"
+
+/* A sequence of instructions that can be compiled all at once */
+typedef struct instr_seq {
+    /* info about where the instruction came from */
+    struct loc_info {
+        /* source code location (for error reporting) */
+        SrcLoc location;
+        /* byte index in the file of the start of the sequence */
+        size_t start;
+        /* byte index in the file of the end of the sequence */
+        size_t end;
+    } source;
+
+    /* tag uesed to indicate effect of this sequence. */
+    enum instr_seq_tag {
+        /* set a cell to `data.byte.value` */
+        ISEQ_SET_CELL,
+        /* `[` instruction */
+        ISEQ_LOOP_OPEN,
+        /* `]` instruction */
+        ISEQ_LOOP_CLOSE,
+        /* `,` instruction */
+        ISEQ_READ,
+        /* `.` instruction */
+        ISEQ_WRITE,
+        /* `+` and `-` can combine, as can `<` and `>`, so combinable
+         * instruction pairs have the same absolute value. */
+        /* data[0] will be */
+        /* A sequence of instructions equivalent in effect to `+` repeated
+         * `count & 0xff` times. */
+        ISEQ_ADD,
+        /* A sequence of instructions equivalent in effect to `>` repeated
+         * `count & 0xff` times. */
+        ISEQ_MOVE_RIGHT,
+        /* A sequence of instructions equivalent in effect to `-` repeated
+         * `count` times. */
+        ISEQ_SUB,
+        /* A sequence of instructions equivalent in effect to `<` repeated
+         * `count` times. */
+        ISEQ_MOVE_LEFT,
+    } tag;
+
+    /* Data that some sequence types need. Meaning depends on `tag`, and is
+     * explained in docstrings for the different variants. */
+    u64 count;
+
+} InstrSeq;
+
+union opt_result {
+    struct {
+        size_t len;
+        InstrSeq *instrs;
+    } output;
+
+    BFCError *err;
+};
+
+nonnull_args bool optimize_instructions(
+    const char *restrict code, size_t size, union opt_result *result
+);
+
 /* filter out all non-BF bytes, and anything that is trivially determined to be
  * dead code, or code with no effect (e.g. "+-" or "<>"), and replace "[-]" and
  * "[+]" with "@".
