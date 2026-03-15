ff9cd26e465d7044244c01fc88610a4a1d2a97d7
Eli Array Minkoff <eli@planetminkoff.com>
2026-03-15 18:37:18 -0400

    Skip disassembly tests if LLVM didn't load

--- backend_i386.c
+++ backend_i386.c
@@ -319,15 +319,15 @@ static void test_inc_dec_is_32_bit(void) {
 CU_pSuite register_i386_tests(void) {
     CU_pSuite suite;
     INIT_SUITE(suite);
-    ADD_TEST(suite, test_set_reg);
-    ADD_TEST(suite, test_jump_instructions);
-    ADD_TEST(suite, test_add_sub_small_imm);
-    ADD_TEST(suite, test_add_sub_medium_imm);
-    ADD_TEST(suite, test_add_sub_large_imm);
-    ADD_TEST(suite, test_add_sub_byte);
-    ADD_TEST(suite, test_set_cell);
+    ADD_DISASM_TEST(suite, test_set_reg);
+    ADD_DISASM_TEST(suite, test_jump_instructions);
+    ADD_DISASM_TEST(suite, test_add_sub_small_imm);
+    ADD_DISASM_TEST(suite, test_add_sub_medium_imm);
+    ADD_DISASM_TEST(suite, test_add_sub_large_imm);
+    ADD_DISASM_TEST(suite, test_add_sub_byte);
+    ADD_DISASM_TEST(suite, test_set_cell);
     ADD_TEST(suite, test_jump_too_long);
-    ADD_TEST(suite, test_inc_dec_is_32_bit);
+    ADD_DISASM_TEST(suite, test_inc_dec_is_32_bit);
     return (suite);
 }
 
